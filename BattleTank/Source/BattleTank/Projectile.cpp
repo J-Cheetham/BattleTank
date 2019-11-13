1816f1cafd61e2c7a540be9be19d471855f49e1c
J_Cheetham <j20.jc@hotmail.com>
2019-11-13 07:34:13 +0000

    Spawning spring onto tank. Spring needs to be tuned

--- BattleTank/Source/BattleTank/Projectile.cpp
+++ BattleTank/Source/BattleTank/Projectile.cpp
@@ -14,8 +14,6 @@ AProjectile::AProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-	
-
 	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
 	SetRootComponent(CollisionMesh);
 	CollisionMesh->SetNotifyRigidBodyCollision(true);
